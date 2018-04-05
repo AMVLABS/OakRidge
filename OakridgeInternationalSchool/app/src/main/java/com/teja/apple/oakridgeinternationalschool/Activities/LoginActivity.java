c5284a1e375a430b722da79c13e29bf78310b366
tejaamvlabs <tejaamvlabs@gmail.com>
2018-04-05 11:13:23 +0530

    teja/MySchool

---
+++ OakridgeInternationalSchool/app/src/main/java/com/teja/apple/oakridgeinternationalschool/Activities/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.teja.apple.oakridgeinternationalschool.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
+
+import com.teja.apple.oakridgeinternationalschool.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button b1=(AppCompatButton)findViewById(R.id.btLogin);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button b2=(AppCompatButton)findViewById(R.id.btSignUp);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ OakridgeInternationalSchool/app/src/main/java/com/teja/apple/oakridgeinternationalschool/Activities/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.teja.apple.oakridgeinternationalschool.Activities;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.teja.apple.oakridgeinternationalschool.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
+
+
+
+
